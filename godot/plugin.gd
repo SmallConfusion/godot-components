b3d50227ed25815d1e3b19c89283bedaf88665cb
SmallConfusion <smallconfusion@protonmail.com>
2026-04-05 18:23:07 -0500

    Working on components plugin

---
+++ godot/plugin.gd
@@ -0,0 +1,17 @@
+@tool
+extends EditorPlugin
+
+var autoloads: Array[Array] = [
+    [InputMapManager, "./keybind_menu/input_map_manager.gd"],
+    [Settings, "./settings/settings.gd],
+]
+
+
+func _enable_plugin():
+    for autoload in autoloads:
+        add_autoload_singleton(autoload[0], autoload[1])
+
+
+func _disable_plugin():
+    for autoload in autoloads:
+    	remove_autoload_singleton(autoload[0])
