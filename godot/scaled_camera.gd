2725671d3828ab4d988d34af520c6821a006fe01
SmallConfusion <smallconfusion@protonmail.com>
2026-04-11 13:24:22 -0500

    Fix scaling, change logo

--- godot/scaled_camera.gd
+++
@@ -1,8 +0,0 @@
-class_name ScaledCamera
-extends Camera2D
-
-@export var base_zoom := 1.0
-
-func _process(_delta: float) -> void:
-	zoom = Vector2.ONE * base_zoom / get_window().content_scale_factor
-	
